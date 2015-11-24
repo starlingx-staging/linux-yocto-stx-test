9cf2f1edd9a5563492daac98a5e12cce303f1124
Chris Friesen <chris.friesen@windriver.com>
2015-11-24 16:27:28 -0500

    affine compute kernel threads
    This is a kernel enhancement to configure the cpu affinity of kernel
    threads via kernel boot option kthread_cpus=<cpulist>. The compute
    kickstart file and compute-huge.sh scripts will update grub with the
    new option.
    
    With kthread_cpus specified, the cpumask is immediately applied upon
    thread launch. This does not affect kernel threads that specify cpu
    and node.
    
    Note: this is based off of Christoph Lameter's patch at
    https://lwn.net/Articles/565932/ with the only difference being
    the kernel parameter changed from kthread to kthread_cpus.
    
    Signed-off-by: Christoph Lameter <cl@linux.com>
    Signed-off-by: Chris Friesen <chris.friesen@windriver.com>
    [VT: The existing "isolcpus"
     kernel bootarg, cgroup/cpuset, and taskset might provide the some
     way to have cpu isolation.  However none of them satisfies the requirements.
     Replacing spaces with tabs. Combine two calls of set_cpus_allowed_ptr()
     in kernel_init_freeable() in init/main.c into one.  Performed tests]
    Signed-off-by: Vu Tran <vu.tran@windriver.com>
    
    Signed-off-by: Jim Somerville <Jim.Somerville@windriver.com>
    Signed-off-by: Zhang Zhiguo <zhangzhg@neusoft.com>
    Signed-off-by: Vefa Bicakci <vefa.bicakci@windriver.com>
    [jm: Adapted the patch for context changes.]
    Signed-off-by: Jiping Ma <jiping.ma2@windriver.com>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -405,8 +405,7 @@ struct task_struct *__kthread_create_on_node(int (*threadfn)(void *data),
 		 * The kernel thread should not inherit these properties.
 		 */
 		sched_setscheduler_nocheck(task, SCHED_NORMAL, &param);
-		set_cpus_allowed_ptr(task,
-				     housekeeping_cpumask(HK_FLAG_KTHREAD));
+		set_cpus_allowed_ptr(task, cpu_kthread_mask);
 	}
 	kfree(create);
 	return task;
@@ -655,7 +654,7 @@ int kthreadd(void *unused)
 	/* Setup a clean context for our children to inherit. */
 	set_task_comm(tsk, "kthreadd");
 	ignore_signals(tsk);
-	set_cpus_allowed_ptr(tsk, housekeeping_cpumask(HK_FLAG_KTHREAD));
+	set_cpus_allowed_ptr(tsk, cpu_kthread_mask);
 	set_mems_allowed(node_states[N_MEMORY]);
 
 	current->flags |= PF_NOFREEZE;
