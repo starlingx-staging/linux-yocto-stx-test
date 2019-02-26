e3636fd59065515b4f32c7f4d24aef13990b62cc
John Jacques <john.jacques@intel.com>
2019-02-25 21:47:57 -0800

    misc: Add drivers for Axxia MISC devices
    This contains the drivers need to support the MISC devices on the
    Intel AXXIA board:
    
    axxia-fault: enabled with AXXIA_FAULT kernel option
      Add fault handlers for system aborts
    
    axxia-mdio: enabled with AXXIA_MDIO kernel option
      Provide access to the Axxia MDIO bus.
    
    axxia-mtc: enabled with AXXIA_MTC kernel option
      The Master Test Controller (MTC) is a test generator that is fully
      compliant with IEEE 1149.1 and can run JTAG test sequences on
      external devices.
    
    axxia-ncr: enabled with AXXIA_NCR kernel option
      Provides access to the INTEL Axxia NCR bus.
    
    axxia-oem: enabled with AXXIA_OEM kernel option
      OEM calls to the secure monitor for Axxia.
      Add the ability to control the ACTLR_EL3 and ACTLR_EL2 registers
      on Axxia using OEM calls to the secure monitor. As this driver
      now does more than just control DSP clusters, the name has been
      changed.
    
    axxia-pei: enabled with AXXIA_PEI kernel option
      Set up the PEI controllers in Linux instead of the boot loader.
    
    axxia-smmon: enabled with AXXIA_SMMON kernel option
      Monitor the system memory controllers for errors.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>
    Signed-off-by: Gary McGee <gary.mcgee@intel.com>
    Signed-off-by: Charlie Paul <charlie.paul@windriver.com>
    Signed-off-by: Daniel Dragomir <daniel.dragomir@windriver.com>

---
+++ include/linux/axxia-oem.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0
+ * Copyright (C) 2016 Intel <john.jacques@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __DRIVERS_MISC_AXXIA_DSPC_H
+#define __DRIVERS_MISC_AXXIA_DSPC_H
+
+/*
+ * DSP Cluster Control -- Only on 6700
+ */
+
+unsigned long axxia_dspc_get_state(void);
+void axxia_dspc_set_state(unsigned long a);
+
+/*
+ * ACTLR_EL3/ACTLR_EL2 Access -- For Performance Testing
+ */
+
+unsigned long axxia_actlr_el3_get(void);
+void axxia_actlr_el3_set(unsigned long a);
+unsigned long axxia_actlr_el2_get(void);
+void axxia_actlr_el2_set(unsigned long a);
+
+/*
+ * CCN504 (5600)
+ * CCN512 (6700)
+ *
+ * 64 bit registers.
+ */
+
+unsigned long axxia_ccn_get(unsigned int offset);
+void axxia_ccn_set(unsigned int offset, unsigned long value);
+
+#endif /* __DRIVERS_MISC_AXXIA_DSPC_H */
