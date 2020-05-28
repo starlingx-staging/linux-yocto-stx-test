091c34540306b15f4fa0a321c0e6bdaeacc42648
Adrian Hunter <adrian.hunter@intel.com>
2020-05-28 15:08:58 +0300

    perf record: Respect --no-switch-events
    commit 16b4b4e1a0038365a6734a4f50aba77e57865c8e upstream.
    
    Context switch events are added automatically by Intel PT and Coresight.
    
    Make it possible to suppress them. That is useful for tracing the
    scheduler without the disturbance that the switch event processing
    creates.
    
    Example:
    
      Prerequisites:
    
        $ which perf
        ~/bin/perf
        $ sudo setcap "cap_sys_rawio,cap_sys_admin,cap_sys_ptrace,cap_syslog,cap_ipc_lock=ep" ~/bin/perf
        $ sudo chmod +r /proc/kcore
    
      Before:
    
        $ perf record --no-switch-events --kcore -a -e intel_pt//k -- sleep 0.001
        [ perf record: Woken up 1 times to write data ]
        [ perf record: Captured and wrote 0.938 MB perf.data ]
        $ perf script -D | grep PERF_RECORD_SWITCH | wc -l
        572
    
      After:
    
        $ perf record --no-switch-events --kcore -a -e intel_pt//k -- sleep 0.001
        Warning:
        Intel Processor Trace decoding will not be possible except for kernel tracing!
        [ perf record: Woken up 1 times to write data ]
        [ perf record: Captured and wrote 0.838 MB perf.data ]
        $ perf script -D | grep PERF_RECORD_SWITCH | wc -l
        0
    
        $ sudo chmod go-r /proc/kcore
        $ sudo setcap -r ~/bin/perf
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Link: http://lore.kernel.org/lkml/20200528120859.21604-1-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- tools/perf/util/record.h
+++ tools/perf/util/record.h
@@ -35,6 +35,7 @@ struct record_opts {
 	bool	      auxtrace_sample_mode;
 	bool	      record_namespaces;
 	bool	      record_switch_events;
+	bool	      record_switch_events_set;
 	bool	      all_kernel;
 	bool	      all_user;
 	bool	      kernel_callchains;
@@ -72,5 +73,10 @@ extern const char * const *record_usage;
 extern struct option *record_options;
 
 int record__parse_freq(const struct option *opt, const char *str, int unset);
+
+static inline bool record_opts__no_switch_events(const struct record_opts *opts)
+{
+	return opts->record_switch_events_set && !opts->record_switch_events;
+}
 
 #endif // _PERF_RECORD_H
